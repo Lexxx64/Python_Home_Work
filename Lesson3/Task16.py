42a2c8536ba0f047706c453c7ae08002125042e1
Lexxx <simankovich@inbox.ru>
2023-02-28 19:15:33 +0400

    внесены исправления в задачу 16

--- Lesson3/Task16.py
+++ Lesson3/Task16.py
@@ -6,7 +6,7 @@ list = [randint(1, 10) for i in range(n)]
 print(list)
 count = 0
 x = int(input('Введите проверяемое число: '))
-for i in list:
+for i in range(n):
     if list[i] == x:
         count += 1
 print(list)
