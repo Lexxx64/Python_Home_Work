7ee9d102f3bc173e4b44fd74a406d85f3fdde9e4
Lexxx <simankovich@inbox.ru>
2023-03-03 18:07:31 +0400

    значительно упрощена задача 22 после рецензии

--- Lesson4/Task22.py
+++ Lesson4/Task22.py
@@ -1,8 +1,4 @@
 list_1 = list(map(int, input('Введите числа через пробел: ').split()))
 list_2 = list(map(int, input('Введите числа через пробел: ').split()))
-new_list = set()
+print(sorted(set(list_1) & set(list_2)))
 
-for i in list_1:
-    if i in list_2:
-        new_list.add(i)
-print(sorted(new_list))
