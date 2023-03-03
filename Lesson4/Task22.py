03369eb2b6d1557c64fd75998efc5abe99118e19
Lexxx <simankovich@inbox.ru>
2023-03-03 18:50:00 +0400

    внесены иправления задачи 24 после рецензии

--- Lesson4/Task22.py
+++ Lesson4/Task22.py
@@ -1,4 +1,3 @@
 list_1 = list(map(int, input('Введите числа через пробел: ').split()))
 list_2 = list(map(int, input('Введите числа через пробел: ').split()))
 print(sorted(set(list_1) & set(list_2)))
-
--- Lesson4/Task24.py
+++ Lesson4/Task24.py
@@ -3,11 +3,11 @@ from random import randint
 number_of_beds = int(input('Введите количество грядок: '))
 number_of_fruits = [randint(0, 10) for i in range(number_of_beds)]
 print(number_of_fruits)
-max_sum_blueberries = sum(number_of_fruits[:3])
+sum_sector_blueberries = 0
+sum_blueberries = set()
 
 for i in number_of_fruits:
     number_of_fruits.insert(0, number_of_fruits.pop())
     sum_sector_blueberries = sum(number_of_fruits[:3])
-    if max_sum_blueberries < sum_sector_blueberries:
-        max_sum_blueberries = sum_sector_blueberries
-print(max_sum_blueberries)
+    sum_blueberries.add(sum_sector_blueberries)
+print(max(sum_blueberries))
