5702e338f697aa970ea11e784233ef7890920936
Lexxx <simankovich@inbox.ru>
2023-03-24 00:09:13 +0400

    fixed all def

--- Seminar8/main.py
+++ Seminar8/main.py
@@ -1,26 +1,25 @@
-from modul import *
-
-print('режимы работы\n'
-      '1 - создать новый контакт\n'
-      '2 - найти контакт\n'
-      '3 - изменить или удалить контакт\n'
-      '4 - просмотр телефонной книги\n'
-      '5 - нумератор\n'
-      '0 - выход')
+from modul import * # noqa
 
 
 while True:
+    print('режимы работы\n'
+          '1 - создать новый контакт\n'
+          '2 - найти контакт\n'
+          '3 - изменить или удалить контакт\n'
+          '4 - просмотр телефонной книги\n'
+          '5 - нумератор\n'
+          '0 - выход')
     mode = input('Выбор режима работы: ')
     if mode == '1':
-        create_data()
+        rec_contact() # noqa
     elif mode == '2':
-        search_user()
+        search_user() # noqa
     elif mode == '3':
-        print(ren_del_user())
+        print(ren_del_user()) # noqa
     elif mode == '4':
-        print(read_data())
+        print(read_data()) # noqa
     elif mode == '5':
-        print(numerate())
+        print(numerate()) # noqa
     elif mode == '0':
         break
     else:
--- Seminar8/modul.py
+++ Seminar8/modul.py
@@ -1,54 +1,59 @@
-def create_data():
-    with open('Seminar8/data.txt', 'r+', encoding='utf-8') as book:
-        data = list(str(book.read()).split('\n'))
-        last_name = input('Введите фамилию: ').title()
-        first_name = input('Имя: ').title()
-        patronymic = input('Отчество: ').title()
-        number = input('номер телефона: ')
-        for el in data:
-            if number in el:
-                return print('Такой номер существует')
-        book.write(f'{last_name} {first_name} {patronymic} {number}\n')
+import time
+TXTFILE = 'Seminar8/data.txt'
+
+
+def new_contact():
+    last_name = input('Введите фамилию: ').title()
+    first_name = input('Имя: ').title()
+    patronymic = input('Отчество: ').title()
+    number = input('номер телефона: ')
+    contact = (f'{last_name} {first_name} {patronymic} {number}')
+    return contact
+
+
+def rec_contact():
+    with open(TXTFILE, 'a', encoding='utf-8') as book:
+        book.write(f'{new_contact()}\n')
 
 
 def read_data():
-    with open('Seminar8/data.txt', 'r', encoding='utf-8') as book:
+    with open(TXTFILE, 'r', encoding='utf-8') as book:
         data = list(str(book.read()).split('\n'))
-        return '\n'.join(data)
-
-
-def numerate():
-    with open('Seminar8/data.txt', 'r', encoding='utf-8') as book:
-        for i, line in enumerate(book, start=1):
-            print(i, line)
+        for i, line in enumerate(data, start=1):
+            if line.strip():
+                print(i, line)
 
 
 def search_user():
-    with open('Seminar8/data.txt', 'r+', encoding='utf-8') as book:
+    with open(TXTFILE, 'r', encoding='utf-8') as book:
         data = list(str(book.read()).split('\n'))
         searching_data = input('Введите данные поиска: ')
         # user = list()
         for pos, el in enumerate(data):
             if searching_data.lower() in el.lower():
                 print(pos, el)
-            return 'Такого пользователя нет'
-        index = int(input('Введите соответсвующий номер строки: '))
+        index = int(input('Введите соответствующий номер строки: '))
         action = input('Выберите действие:\n'
                        'd - удалить контакт\n'
                        'r - изменить контакт\n'
                        'o - я чисто посмотреть\n')
         if action == 'd':
             del data[index]
-            with open('Seminar8/data.txt', 'w', encoding='utf-8') as book:
+            print(f'Контакт {data[index]} удалён!')
+            with open(TXTFILE, 'w', encoding='utf-8') as book:
                 for line in data:
                     book.write(f'{str(line)}\n')
-        # user.append(el)
-        # choice = input('хотите изменитть контакт ')
-        # if choice == 'y':
-        #     return ren_del_user()
-    # print('\n'.join(user))
+        time.sleep(1)
+        if action == 'r':
+            rename_contact = new_contact()
+            print(f'Контакт {data[index]} изменён!')
+            data[index] = rename_contact
+            with open(TXTFILE, 'w', encoding='utf-8') as book:
+                for line in data:
+                    book.write(f'{str(line)}\n')
+        time.sleep(1)
 
 
 def ren_del_user():
     print(search_user())
-
+    pass
